09d9b253f2ebf08acbcf3723331d8bc4c6b80015
Matthew M. Keeler <keelerm84@gmail.com>
2021-04-28 11:34:42 -0400

    feat: Add structopt structure for future flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use structopt::StructOpt;
+
+/// Connect to AWS EC2 hosts via a Bastion / Jump host
+#[derive(StructOpt)]
+#[structopt(name = "heimdallr")]
+struct Heimdallr {
+    /// Profile name as specified in your configuration file
+    #[structopt(name = "profile", long, short = "p")]
+    profile: Option<String>,
+
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    /// List all running instances
+    List,
+
+    /// Add your IP to a security group to allow ingress
+    Grant {
+        /// Descriptive text to include with your security group entry
+        #[structopt(name = "description", long, short = "d")]
+        description: Option<String>,
+    },
+
+    /// Revoke your IP from a security group to prevent future ingress
+    Revoke,
+
+    /// Connect to a running instance
+    Connect,
+
+    /// Update this executable to the latest version
+    Update,
+}
+
+fn main() -> Result<()> {
+    let opt = Heimdallr::from_args();
+    Ok(())
 }
