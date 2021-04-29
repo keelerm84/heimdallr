4c50c0f5f42859a2e40f88f79311c67f8a8443a2
Matthew M. Keeler <keelerm84@gmail.com>
2021-04-28 22:43:43 -0400

    feat: Implement grant and revoke commands

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,21 @@ enum Command {
 
     /// Add your IP to a security group to allow ingress
     Grant {
+        /// The security group id that controls ingress to the bastion server
+        #[structopt(name = "security-group-id", long, short = "s")]
+        security_group_id: String,
+
         /// Descriptive text to include with your security group entry
         #[structopt(name = "description", long, short = "d")]
         description: Option<String>,
     },
 
     /// Revoke your IP from a security group to prevent future ingress
-    Revoke,
+    Revoke {
+        /// The security group id that controls ingress to the bastion server
+        #[structopt(name = "security-group-id", long, short = "s")]
+        security_group_id: String,
+    },
 
     /// Connect to a running instance
     Connect,
@@ -43,6 +51,11 @@ async fn main() -> Result<()> {
 
     match opt.cmd {
         Command::List => cmd::list::list_running_instances().await,
+        Command::Grant {
+            security_group_id,
+            description,
+        } => cmd::access::grant(security_group_id, description).await,
+        Command::Revoke { security_group_id } => cmd::access::revoke(security_group_id).await,
         _ => Ok(()),
     }
 }
